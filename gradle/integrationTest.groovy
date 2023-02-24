d6c7a03eb17233648ce97b214c6f1178b6f5038b
Jakob Holst <jho@miracle.dk>
2023-02-24 09:39:49 +0100

    Revert "in test add in gra"
    This reverts commit 213b74a053ffc2f7b6a741828b99d9a8442e3ee8.

--- gradle/integrationTest.groovy
+++ gradle/integrationTest.groovy
@@ -1,5 +1,21 @@
-integrationTest.doFirst {
-    delete "${rootDir}/build"
-    delete "${rootDir}/cesop_test.log"
+sourceSets {
+    integrationTest {
+        groovy.srcDir "$projectDir/src/integration-test/groovy"
+        resources.srcDir "$projectDir/src/integration-test/resources"
+        compileClasspath += sourceSets.main.output + sourceSets.test.output
+        runtimeClasspath += sourceSets.main.output + sourceSets.test.output
+    }
 }
 
+configurations {
+    integrationTestImplementation.extendsFrom testImplementation
+    integrationTestRuntimeOnly.extendsFrom testRuntimeOnly
+    integrationTestApi.extendsFrom testApi
+}
+
+task integrationTest(type: Test) {
+    group = 'verification'
+    testClassesDirs = sourceSets.integrationTest.output.classesDirs
+    classpath = sourceSets.integrationTest.runtimeClasspath
+    outputs.upToDateWhen { false }
+}
